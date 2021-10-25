a956e16c1ad13ffdd9fafec1203d6fb70a326c5e
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-25 14:39:15 +0300

    1Commit

---
+++ src/components/calendar/Calendar.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+import Navigation from "./../navigation/Navigation";
+import Week from "../week/Week";
+import Sidebar from "../sidebar/Sidebar";
+import events from "../../gateway/events";
+
+import "./calendar.scss";
+
+function Calendar({ weekDates }) {
+  const [event, setEvent] = useState(events);
+  return (
+    <section className="calendar">
+      <Navigation weekDates={weekDates} />
+      <div className="calendar__body">
+        <div className="calendar__week-container">
+          <Sidebar />
+          <Week weekDates={weekDates} events={event} />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Calendar;
