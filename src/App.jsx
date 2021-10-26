b3835aa3e017d5cc3a58b6aa0cdec92142248035
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-26 12:02:43 +0300

    qw

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,31 @@ import Header from "./components/header/Header.jsx";
 import Calendar from "./components/calendar/Calendar.jsx";
 
 import { getWeekStartDate, generateWeekRange } from "../src/utils/dateUtils.js";
-
 import "./common.scss";
 
 function App() {
   const [weekStartDate, setWeekStartDate] = useState(new Date());
   const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
+  
+ const nextWeek = () =>{
+    setWeekStartDate(
+      new Date(weekStartDate.setDate(new Date(weekDates).getDate() +7))
+    )
+  }
 
+ const prevWeek = () =>{
+    setWeekStartDate(
+      new Date(weekStartDate.setDate(new Date(weekDates).getDate() -7))
+    )
+  }
+  const today = () =>{
+    setWeekStartDate(
+      new Date()
+    )
+  }
   return (
     <>
-      <Header />
+      <Header nextWeek={nextWeek} prevWeek={prevWeek} today={today}/>
       <Calendar weekDates={weekDates} />
     </>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import CreateBtn from "./CreateBtn"
-import HeaderNavigation from "./HeaderNavigation"
-
-import { months } from "../../utils/dateUtils.js";
+import  moment from 'moment';
+import { months} from "../../utils/dateUtils.js";
 
 import "./header.scss";
 
-const Header = () => {
+const Header = ({nextWeek,prevWeek,today}) => {
+
   return (
     <header className="header">
-      <CreateBtn />
-      <HeaderNavigation />
+      <button className="button create-event-btn">
+        <i className="fas fa-plus create-event-btn__icon"></i>Create
+      </button>
+      <div className="navigation">
+        <button className="navigation__today-btn button" onClick={today}>{moment().format('dddd')}</button>
+        <button className="icon-button navigation__nav-icon">
+          <i className="fas fa-chevron-left" onClick={prevWeek}></i>
+        </button>
+        <button className="icon-button navigation__nav-icon">
+          <i className="fas fa-chevron-right"onClick={nextWeek}></i>
+        </button>
+        <span className="navigation__displayed-month">{moment().format('MMMM')}</span>
+      </div>
     </header>
   );
 };
